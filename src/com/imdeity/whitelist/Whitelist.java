df6cf50f1a2a57f7cb55fcfc5a8cba98313af634
Ryan van Zeben <vanZeben@live.ca>
2011-08-24 20:10:48 -0400

    Added Whitelist Command /whitelist

--- src/com/imdeity/whitelist/Whitelist.java
+++ src/com/imdeity/whitelist/Whitelist.java
@@ -6,34 +6,61 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Whitelist extends JavaPlugin {
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+public class Whitelist extends JavaPlugin implements CommandExecutor {
 
     protected final Logger log = Logger.getLogger("Minecraft");
 
     private static WhitelistPlayerListener playerListener = null;
     public static MySQLConnection database = null;
+    public static PermissionHandler permissions = null;
 
     @Override
     public void onEnable() {
 
         loadSettings();
-
+        checkPlugins();
         database = new MySQLConnection(this);
         playerListener = new WhitelistPlayerListener(this);
         getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN,
                 playerListener, Priority.Low, this);
 
+        getCommand("whitelist").setExecutor(this);
         out(this.getDescription().getVersion() + " Enabled");
+
     }
 
     @Override
     public void onDisable() {
         out(this.getDescription().getVersion() + " Disabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd,
+            String commandLabel, String args[]) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (permissions.has(player, "whitelist")) {
+                parseCommand(player, args);
+                return true;
+            }
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public void out(String message) {
@@ -63,6 +90,14 @@ public class Whitelist extends JavaPlugin {
         return true;
     }
 
+    private void checkPlugins() {
+        Plugin tmp;
+        tmp = getServer().getPluginManager().getPlugin("Permissions");
+
+        if (tmp != null)
+            permissions = ((Permissions) tmp).getHandler();
+    }
+
     public boolean isOnWhitelist(String playerName) {
         String sql = "";
 
@@ -77,4 +112,30 @@ public class Whitelist extends JavaPlugin {
         return false;
     }
 
+    private void parseCommand(Player player, String[] args) {
+        if (args.length == 1) {
+            if (isOnWhitelist(args[0])) {
+                player.sendMessage("[WhiteList] " + ChatColor.RED + args[0]
+                        + " is already whitelisted.");
+                return;
+
+            }
+            writeToMySQL(args[0]);
+            getServer().broadcastMessage(
+                    ChatColor.GRAY + "[" + ChatColor.RED + "*ImDeity*"
+                            + ChatColor.GRAY + "] " + ChatColor.YELLOW
+                            + args[0] + ChatColor.WHITE + " whitelisted by "
+                            + ChatColor.AQUA + player.getName());
+        } else {
+            player.sendMessage("[Whitelist] " + ChatColor.RED
+                    + "You need to enter a name.");
+        }
+    }
+
+    private void writeToMySQL(String name) {
+        String sql = "";
+
+        sql = ("INSERT INTO `whitelist` " + " (`name`) " + " VALUES (?);");
+        database.Write(sql, name);
+    }
 }
